c33ccd71e1751d94772ab72e0c68d4e30709f8dc
Simon Kilby <simon.kilby@aecom.com>
2023-12-14 11:28:59 +0000

    removed very long line

--- .github/workflows/sample_python_file.py
+++ .github/workflows/sample_python_file.py
@@ -21,4 +21,4 @@ thing = (
     ' blah'
     )
 
-thing = 'really long pairs of strings that extend a long way along a line and should be split ' + 'really long pairs of strings that extend a long way along a line and should be split '
+
