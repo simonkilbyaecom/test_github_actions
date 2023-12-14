2e96b92298f527399de459d32620acf93a5b6cb1
Simon Kilby <simon.kilby@aecom.com>
2023-12-14 11:27:15 +0000

    submited unformated file

---
+++ .github/workflows/sample_python_file.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+import polars
+
+
+
+from polars import from_arrow
+
+
+FOO = 'some string'
+
+BAR = 1254
+
+thing = (
+    'stuff ' + 
+    ' thing ' + 
+    ' blah'
+    )
+
+thing = 'really long pairs of strings that extend a long way along a line and should be split ' + 'really long pairs of strings that extend a long way along a line and should be split '
